6d0dfb702ed96b11683279af43aa147049aa09cf
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-10-28 23:47:07 -0600

    Agregar archivos App.css y App.tsx con la estructura inicial de la aplicación React

---
+++ Clase1/src/App.tsx
@@ -0,0 +1,15 @@
+import InputDinamico from './components/inputDinamico'
+import Contador from './components/contador'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <InputDinamico />
+      <Contador />
+    </>
+  )
+}
+
+export default App
