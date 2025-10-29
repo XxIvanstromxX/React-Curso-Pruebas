bb96d1c9ce9d23ee6ea437aff5228deefde44fbe
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-10-29 17:38:28 -0600

    Agregar comentarios descriptivos al componente Contador

--- Clase1/src/components/contador.tsx
+++ Clase1/src/components/contador.tsx
@@ -1,8 +1,15 @@
+/*
+    Componente Contador en React con TypeScript
+    Este componente implementa un contador simple que permite incrementar su valor al hacer clic en un botón.
+*/
+
 import { useState } from "react";
 
 const Contador = () => {
+    // Estado para el contador
     const [count, setCount] = useState(0);
 
+    // Renderizado del componente
     return (
         <>
             <button onClick={() => setCount(count + 1)}>Incrementar</button>
