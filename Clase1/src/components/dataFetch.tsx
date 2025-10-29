6fbb256c9b99bf2d470e0c396fff9f70b9c3c777
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-10-29 17:40:09 -0600

    Agregar comentarios descriptivos en el componente DataFetch para mejorar la legibilidad del código

--- Clase1/src/components/dataFetch.tsx
+++ Clase1/src/components/dataFetch.tsx
@@ -1,23 +1,32 @@
+/*
+    Componente DataFetch en React con TypeScript
+    Este componente realiza una solicitud a una API para obtener una lista de usuarios y mostrarlos en pantalla.
+*/
+
 import { useEffect, useState } from "react";
 
+// Definición de la interfaz para los datos esperados
 interface datosEsperados {
     name: string
 }
 
 const DataFetchComponent = () => {
-    const [usuarios, setUsuarios] = useState<string[]>([]);
-    const [cargando, setCargando] = useState(true)
+    const [usuarios, setUsuarios] = useState<string[]>([]); // Estado para almacenar los nombres de los usuarios
+    const [cargando, setCargando] = useState(true) // Estado para indicar si los datos están siendo cargados
 
+    // useEffect para realizar la solicitud a la API cuando el componente se monta
     useEffect(() => {
         const fetchData = async () => {
             try{
+                // Realizar la solicitud a la API de prueba
                 const response = await fetch("https://jsonplaceholder.typicode.com/users")
                 const data = await response.json();
                 const nombre = data.map((d: datosEsperados) => { return d.name})
                 console.log(nombre)
 
+                // Actualizar el estado con los nombres de los usuarios y cambiar el estado de cargando
                 setUsuarios(nombre)
-                setCargando(false)
+                setCargando(false) // Datos cargados
             }
             catch(e){
                 console.error(e);
@@ -27,6 +36,7 @@ const DataFetchComponent = () => {
         fetchData();
     }, []);
 
+    // Renderizado condicional basado en el estado de cargando y la lista de usuarios
     if(cargando){
         return <h2>Cargando...</h2>
     }
