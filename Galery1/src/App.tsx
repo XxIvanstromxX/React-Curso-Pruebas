0f56caa4f34ebde7a493a49c54155d5b1301f981
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-10-30 00:28:53 -0600

    Agregar componente App con título y GaleriaApp

---
+++ Galery1/src/App.tsx
@@ -0,0 +1,12 @@
+import GaleriaApp from "./components/GaleryApp"
+
+function App() {
+  return (
+    <>
+      <h1>Galería de Imágenes</h1>
+      <GaleriaApp/>
+    </>
+  )
+}
+
+export default App
