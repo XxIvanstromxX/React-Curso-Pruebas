c623f5e72cd11b0c8d8c9da7444df786b81bddb9
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-11-01 00:40:25 -0600

    Agregar funcionalidad para seleccionar y mostrar detalles de un personaje en GaleriaApp

--- Galery1/src/components/GaleryApp.tsx
+++ Galery1/src/components/GaleryApp.tsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react';
+import styles from"./Galery.module.css"
 
 interface Image {
     id: number,
@@ -9,6 +10,7 @@ interface Image {
 const GaleriaApp = () => {
     const [fotos, setFotos] = useState<Image[]>([]);
     const [cargando, setCargando] = useState(false);
+    const [personajeSeleccionado, setPersonajeSeleccionado] = useState<Image | null>();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -38,22 +40,49 @@ const GaleriaApp = () => {
         fetchData();
     }, []);
 
+    const seleccionarPersonaje = (personaje: Image) => {
+        setPersonajeSeleccionado(personaje);
+    }
+
     if(cargando){
         return <p>Cargando...</p>
     }
     if(fotos.length === 0){
         return <p>Datos no encontrados...</p>
     }
+    if(personajeSeleccionado != null){
+        return (
+            <div>
+                <h2>{personajeSeleccionado.name}</h2>
+                <img 
+                    src={personajeSeleccionado.image} 
+                    alt={personajeSeleccionado.name} 
+                    width="auto" height="300px"
+                />
+                <br/>
+                <button onClick={() => setPersonajeSeleccionado(null)}>
+                    Volver a la galería
+                </button>
+            </div>
+        )
+    }
 
     return (
         <div>
             <h2>Cargando imagenes {fotos.length}</h2>
-            <ul>
+            <ul className={styles["galery-ul"]}>
                 {
                     fotos.map((foto)=>(
                         <li key={foto.id}>
                             <h4>{foto.name}</h4>
-                            <img src={foto.image} alt={foto.name} width="100px" height="auto"/>
+                            <a onClick={() => seleccionarPersonaje(foto)} href='#'>
+                                <img 
+                                    src={foto.image} 
+                                    alt={foto.name} 
+                                    width="auto" height="150px"
+                                />
+                            </a>
+                           
                         </li>
                     ))
                 }
