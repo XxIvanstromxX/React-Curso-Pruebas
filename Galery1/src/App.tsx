f5674b85383521545122619f3db94554c4565a10
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-11-11 22:20:15 -0600

    Descomentar componente GaleriaApp y comentar su uso en App.tsx

--- Galery1/src/App.tsx
+++ Galery1/src/App.tsx
@@ -1,10 +1,14 @@
-import GaleriaApp from "./components/GaleryApp"
+// import GaleriaApp from "./components/GaleryApp"
+import Boton from "./components/Boton"
+
+const palabras = ["Hola", "Mundo", "Desde", "React", "CCOL"];
 
 function App() {
   return (
     <>
       <h1>Galería de Imágenes</h1>
-      <GaleriaApp/>
+      {/* <GaleriaApp/> */}
+      <Boton palabras={palabras}/>
     </>
   )
 }
