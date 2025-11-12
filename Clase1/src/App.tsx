0fa37ac9376232827ad962a7fec7b311f69f9712
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-11-11 22:20:07 -0600

    Descomentar componente DataFetchComponent y comentar ConversorUnificado en App.tsx

--- Clase1/src/App.tsx
+++ Clase1/src/App.tsx
@@ -2,9 +2,9 @@
 //import Contador from './components/contador'
 //import ComponenteConCleanUp from './components/componenteCleanUp'
 //import Tareas from './components/tareas'
-//import DataFetchComponent from './components/dataFetch'
+import DataFetchComponent from './components/dataFetch'
 //import ConversorUnidades from './components/conversorUnidades'
-import ConversorUnificado from './components/conversorUnificado'
+// import ConversorUnificado from './components/conversorUnificado'
 import './App.css'
 
 function App() {
@@ -18,9 +18,9 @@ function App() {
       <ComponenteConCleanUp />
       <br /> */}
       {/* <Tareas nombre="Aprender React" /> */}
-      {/* <DataFetchComponent/> */}
+      <DataFetchComponent/>
       {/* <ConversorUnidades /> */}
-      <ConversorUnificado />
+      {/* <ConversorUnificado /> */}
     </>
   )
 }
