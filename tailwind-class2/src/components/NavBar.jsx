e886b5821cdfed02fe51ec6cfe10b86ffe0e5e7c
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-20 00:46:58 -0600

    feat: actualizar estilos y reemplazar icono del menú en NavBar

--- tailwind-class2/src/components/NavBar.jsx
+++ tailwind-class2/src/components/NavBar.jsx
@@ -13,7 +13,7 @@ export function NavBar() {
             <h1 className="text-xl font-bold">
                 My Website
             </h1>
-            <ul className="hidden lg:flex absolute left-1/2 -translate-x-1/2 flex-row gap-6 text-sm">
+            <ul className="hidden lg:flex absolute left-1/2 -translate-x-1/2 flex-row gap-6 text-md">
                 <li className="hover:underline cursor-pointer">Home</li>
                 <li className="hover:underline cursor-pointer">About</li>
                 <li className="hover:underline cursor-pointer">Services</li>
@@ -21,10 +21,10 @@ export function NavBar() {
             </ul>
 
             <div className="hidden lg:flex flex-row gap-4">    
-                <button className="bg-white hover:bg-white/90 dark:bg-white/10 dark:hover:bg-white/20 transition-all duration-200 dark:text-white text-gray-800 h-8 px-3 rounded-lg text-sm border-[0.5px] border-gray-300 dark:border-gray-600">
+                <button className="bg-white hover:bg-gray-200 dark:bg-white/10 dark:hover:bg-white/20 transition duration-200 dark:text-white text-gray-800 h-8 px-3 rounded-lg text-sm border-[0.5px] border-gray-300 dark:border-gray-600">
                     Sign Up
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 transition-all duration-200 text-white h-8 px-3 rounded-lg text-sm">
+                <button className="bg-linear-to-br from-amber-700 to-amber-500  hover:bg-linear-to-br hover:from-amber-900 hover:to-amber-700 transition duration-200 text-white h-8 px-3 rounded-lg text-sm">
                     Get Started
                 </button>
                 <DarkModeToggle/>
@@ -32,12 +32,10 @@ export function NavBar() {
 
             <div className="lg:hidden">
                 <button onClick={toggleMenu} className="lg:hidden p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 transition">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
+                    <img src="/hamburger.svg" alt="menu" className="h-6 w-6 dark:invert"/>
                 </button>
                 {isOpen && (
-                    <div className="absolute left-1/2 -translate-x-1/2 bottom-4 bg-white dark:bg-gray-900 shadow-lg rounded-xl w-[95%] py-4 flex flex-col gap-4 z-50 border border-gray-300 dark:border-gray-600">
+                    <div className="absolute left-1/2 -translate-x-1/2 bottom-4 bg-white dark:bg-gray-900 shadow-lg rounded-xl w-[95%] py-4 flex flex-col gap-4 z-50 border border-gray-300 dark:border-gray-600 backdrop-blur-md">
                         <a href="#" className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">Home</a>
                         <a href="#" className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">About</a>
                         <a href="#" className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">Services</a>
