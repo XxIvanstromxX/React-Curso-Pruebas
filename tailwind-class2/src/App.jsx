fb4bd4138670a8ef94d31af460dfc2796a225bb3
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-19 18:29:22 -0600

    feat: agregar componente App con estructura inicial y estilos de Tailwind CSS

---
+++ tailwind-class2/src/App.jsx
@@ -0,0 +1,14 @@
+import { NavBar } from "./components/NavBar"
+import { Hero } from "./components/Hero"
+
+function App() {
+
+  return (
+    <div className="bg-amber-50 dark:bg-gray-800 h-full min-h-screen transition-colors duration-500">
+      <NavBar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
