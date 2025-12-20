9ecc206532f694513eca5333e3c01254890cb403
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-20 00:46:49 -0600

    fix: corregir la importación del componente Hero para reflejar la nueva estructura de carpetas

--- tailwind-class2/src/App.jsx
+++ tailwind-class2/src/App.jsx
@@ -1,10 +1,10 @@
 import { NavBar } from "./components/NavBar"
-import { Hero } from "./components/Hero"
+import { Hero } from "./sections/Hero"
 
 function App() {
 
   return (
-    <div className="bg-amber-50 dark:bg-gray-800 h-full min-h-screen transition-colors duration-500">
+    <div className="bg-radial-[at_50%_1000%] from-amber-600 via-amber-600 to-amber-50 to-100% dark:bg-radial-[at_50%_1000%] dark:from-gray-400 dark:via-gray-600 dark:to-gray-900 h-full min-h-screen transition-colors duration-500">
       <NavBar />
       <Hero/>
     </div>
