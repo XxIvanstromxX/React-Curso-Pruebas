b2858493a21b631e1e4e198aaa71719d1fa3c96f
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-23 00:03:34 -0600

    fix: ajustar el valor de la propiedad 'to' en la clase de fondo para mejorar el diseño

--- tailwind-class3/src/App.jsx
+++ tailwind-class3/src/App.jsx
@@ -4,7 +4,7 @@ import { Hero } from "./components/Hero"
 function App() {
 
   return (
-    <div className="bg-radial-[at_50%_1000%] from-amber-600 via-amber-600 to-amber-50 to-100% dark:bg-radial-[at_50%_1000%] dark:from-gray-400 dark:via-gray-600 dark:to-gray-900 h-full min-h-screen transition-colors duration-500">
+    <div className="bg-radial-[at_50%_1000%] from-amber-600 via-amber-600 to-amber-50 to-95% dark:bg-radial-[at_50%_1000%] dark:from-gray-400 dark:via-gray-600 dark:to-gray-900 min-h-screen transition-colors duration-500">
       <NavBar />
       <Hero/>
     </div>
