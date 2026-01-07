26da8e0766373232f78c330e082e3400ad9bf747
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-01-07 17:52:54 -0600

    Agregar estructura de layout en App.jsx con MainLayout

--- profesional-class1/src/App.jsx
+++ profesional-class1/src/App.jsx
@@ -1,7 +1,12 @@
+import MainLayout from "./components/layout/MainLayout"
 
 function App() {
   return (
     <>
+      <MainLayout>
+        <h1>Bienvenidos a mi aplicación</h1>
+        <p>Esta es una aplicación de ejemplo utilizando React.</p>
+      </MainLayout>
     </>
   )
 }
