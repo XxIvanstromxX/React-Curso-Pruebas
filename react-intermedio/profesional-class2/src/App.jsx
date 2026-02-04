8c53553c91dc6dc4541af819bb68d6ad1d1665fa
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-04 00:07:53 -0600

    Reemplazar el componente MainLayout por DerivateStateGood y DerivateStateBad en App.jsx

--- react-intermedio/profesional-class2/src/App.jsx
+++ react-intermedio/profesional-class2/src/App.jsx
@@ -1,12 +1,13 @@
-import MainLayout from './components/layout/MainLayout';
+import {
+  DerivateStateGood,
+  DerivateStateBad,
+} from './components/errors/DerivateState';
 
 function App() {
   return (
     <>
-      <MainLayout>
-        <h1>Bienvenidos a mi aplicación</h1>
-        <p>Esta es una aplicación de ejemplo utilizando React.</p>
-      </MainLayout>
+      <DerivateStateBad />
+      <DerivateStateGood />
     </>
   );
 }
